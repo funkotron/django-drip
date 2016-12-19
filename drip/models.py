05a4be55953b1bcc20fe498018bdd1ccef535291
Jervis Whitley <jervisw@mailinator.com>
2016-12-20 10:35:59 +1100

    removed dependancy for parsing durations, this is built into django core

--- drip/models.py
+++ drip/models.py
@@ -3,12 +3,9 @@ from datetime import datetime, timedelta
 from django.db import models
 from django.core.exceptions import ValidationError
 from django.conf import settings
+from django.utils.dateparse import parse_duration
 
 from drip.utils import get_user_model
-
-# just using this to parse, but totally insane package naming...
-# https://bitbucket.org/schinckel/django-timedelta-field/
-import timedelta as djangotimedelta
 
 
 class Drip(models.Model):
@@ -137,16 +134,16 @@ class QuerySetRule(models.Model):
         # set time deltas and dates
         if self.field_value.startswith('now-'):
             field_value = self.field_value.replace('now-', '')
-            field_value = now() - djangotimedelta.parse(field_value)
+            field_value = now() - parse_duration(field_value)
         elif self.field_value.startswith('now+'):
             field_value = self.field_value.replace('now+', '')
-            field_value = now() + djangotimedelta.parse(field_value)
+            field_value = now() + parse_duration(field_value)
         elif self.field_value.startswith('today-'):
             field_value = self.field_value.replace('today-', '')
-            field_value = now().date() - djangotimedelta.parse(field_value)
+            field_value = now().date() - parse_duration(field_value)
         elif self.field_value.startswith('today+'):
             field_value = self.field_value.replace('today+', '')
-            field_value = now().date() + djangotimedelta.parse(field_value)
+            field_value = now().date() + parse_duration(field_value)
 
         # F expressions
         if self.field_value.startswith('F_'):
